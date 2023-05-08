fc5379ab90093d0eecb0bcd804c1956b0493dfcc
Agnieszka Stojanik <agsto@7n.com>
2023-05-08 11:36:59 +0200

    EF core package version update

--- EFCoreShadowForeignKey/Model/LeafEntity.cs
+++ EFCoreShadowForeignKey/Model/LeafEntity.cs
@@ -2,7 +2,7 @@
 {
     public class LeafEntity : BaseEntity
     {
-        // Having non-shadow property for foreign key make it work!
+        // Having non-shadow property for foreign key makes it work!
         //public int RootId { get; set; }
         public RootEntity Root { get; set; }
     }
